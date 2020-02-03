c2e792d70dd69158f5932c9615ea462cf3148619
haltonba <haltonba94@gmail.com>
2020-02-03 12:51:33 -0500

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,18 @@
 require 'pry'
 class MP3Importer
 
-    attr_accessor :path
+    attr_accessor :path, files
 
     def initialize (path)
         @path = path
     end
 
     def files
-        @@files = Dir.entries("./spec/fixtures/mp3s").select {|f| !File.directory? f}
+        @files = Dir.entries("./spec/fixtures/mp3s").select {|f| !File.directory? f}
     end
 
     def import
-        @@files.each do |x|
+        @files.each do |x|
             Song.new_by_filename(x)
         end
     end
